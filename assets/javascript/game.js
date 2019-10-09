66e491037a9a703613455518c16ba40c591c7b95
elpat7 <aguilarpat7@gmail.com>
2019-10-09 16:21:43 -0700

    adding on click gem funtionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,33 @@
 $(document).ready(function () {
-    $('#btn').on(('click', function () {
-        $('.btn.container').append('<button id="wut">Hello</button>');
-    }))
+    $('.gem1').on('click', function () {
+        // $('H1').hide();
+        console.log('hi gem 1');
+
+    });
 })
+
+$(document).ready(function () {
+    $('.gem2').on('click', function () {
+        // $('H1').hide();
+        console.log('hi gem 2');
+
+    });
+})
+
+
+$(document).ready(function () {
+    $('.gem3').on('click', function () {
+        // $('H1').hide();
+        console.log('hi gem 3');
+
+    });
+})
+
+$(document).ready(function () {
+    $('.gem4').on('click', function () {
+        // $('H1').hide();
+        console.log('hi gem 4');
+
+    });
+})
+
