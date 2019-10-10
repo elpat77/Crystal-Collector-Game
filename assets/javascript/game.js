6505f2fc23cfcbb6c4abdbd3fd3bbae0271128c3
elpat7 <aguilarpat7@gmail.com>
2019-10-10 14:46:16 -0700

    adding gem scores and random number match generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,29 @@
+var counter = 0;
+var min = 19;
+var max = 120;
+var match = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log('number to match ' + match);
+
+// var min=19; 
+// var max=120;  
+// var random = Math.random() * (+max - +min) + +min; 
+// document.write("Random Number Generated : " + random );  
 
 $(document).ready(function () {
     $('.gem1').on('click', function () {
-        // $('H1').hide();
-        console.log('hi gem 1');
+        // var counter = Math.floor(Math.random() * 20);
+        counter += 1;
+        alert("Your new score is  " + counter);
+        console.log('your score is ' + counter);
 
     });
 })
 
 $(document).ready(function () {
     $('.gem2').on('click', function () {
-        // $('H1').hide();
-        console.log('hi gem 2');
+        counter += 4;
+        alert("Your new score is  " + counter);
+        console.log('your score is ' + counter);
 
     });
 })
@@ -18,17 +31,20 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     $('.gem3').on('click', function () {
-        // $('H1').hide();
-        console.log('hi gem 3');
+        counter += 7;
+        alert("Your new score is  " + counter);
+        console.log('your score is ' + counter);
 
     });
 })
 
 $(document).ready(function () {
     $('.gem4').on('click', function () {
-        // $('H1').hide();
-        console.log('hi gem 4');
+        counter += 10;
+        alert("Your new score is  " + counter);
+        console.log('your score is ' + counter);
 
     });
 })
+
 
