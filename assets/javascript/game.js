126fa9a07d85e2ae020a6df46ef16d9bbd481e60
elpat7 <aguilarpat7@gmail.com>
2019-10-10 15:02:39 -0700

    adding functions to update scores and #to match

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,15 @@ var max = 120;
 var match = Math.floor(Math.random() * (max - min + 1)) + min;
 console.log('number to match ' + match);
 
-// var min=19; 
-// var max=120;  
-// var random = Math.random() * (+max - +min) + +min; 
-// document.write("Random Number Generated : " + random );  
+$(document).ready(function () {
+    $("#match-update").text(match);
+});
 
 $(document).ready(function () {
     $('.gem1').on('click', function () {
         // var counter = Math.floor(Math.random() * 20);
         counter += 1;
-        alert("Your new score is  " + counter);
+        $("#score-update").text(counter);
         console.log('your score is ' + counter);
 
     });
@@ -22,7 +21,7 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('.gem2').on('click', function () {
         counter += 4;
-        alert("Your new score is  " + counter);
+        $("#score-update").text(counter);
         console.log('your score is ' + counter);
 
     });
@@ -32,7 +31,7 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('.gem3').on('click', function () {
         counter += 7;
-        alert("Your new score is  " + counter);
+        $("#score-update").text(counter);
         console.log('your score is ' + counter);
 
     });
@@ -41,7 +40,7 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('.gem4').on('click', function () {
         counter += 10;
-        alert("Your new score is  " + counter);
+        $("#score-update").text(counter);
         console.log('your score is ' + counter);
 
     });
